a05733897ddcfaa71b8b8bfc0017295b1632b1dc
Steven Klar <klar@bestfewo.de>
2019-10-16 16:18:53 +0200

    Add externalproject comment

--- vendor/premake/projects.lua
+++ vendor/premake/projects.lua
@@ -60,3 +60,10 @@ project "DoorKickers"
         VENDOR_DIR .. "imgui/examples/imgui_impl_win32.*",
         VENDOR_DIR .. "imgui/examples/imgui_impl_opengl2.*",
     }
+
+--[[ Test externalproject
+externalproject "BlackBone"
+   location(VENDOR_DIR .. "Blackbone")
+   uuid "A2C53563-46F5-4D87-903F-3F1F2FDB2DEB"
+   kind "StaticLib"
+]]
